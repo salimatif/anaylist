dc3738450a3ecb304eff6763da512335752a878e
salimatif <atifsalim992@gmail.com>
2025-11-16 20:15:26 +0530

    day 1 doing data

---
+++ atif.py
@@ -0,0 +1,9 @@
+# hay sir please dont delete this line
+print("hello \nworld")
+print("hay i become a \"python devloper\" \nfirstly i will trying to consistantly learn python") 
+print("hello world",8083708992, 992, 992345, sep=" ", end="992\n")
+print("the psyc atif")
+
+#the art of not overthinking
+print("shaurya kapoor the author of the art of not overthinking \nthe art of not overthinking is a great book")
+print("the art of not overthinking" , "is" ,70 , "pages longer book", 100,"fun and understanding book \nfor overthinker" )
