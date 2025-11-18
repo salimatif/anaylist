ec7da7365268af03c21d98a6a146a54f8d0b8d05
salimatif <atifsalim992@gmail.com>
2025-11-18 22:15:33 +0530

    using input

---
+++ day10.py
@@ -0,0 +1,29 @@
+# input function in python
+a = input("enter your name:")
+print("my name is", a)
+
+# case 1
+x = input("enter first number:")
+y = input("enter second number:")
+print(int(x) + int(y))
+
+# case 2
+x = float(input("enter first number:"))
+y = float(input("enter second number:"))
+print(x + y)   
+
+# case 3
+x = str(input
+        ("enter first number:"))
+y = str(input
+        ("enter second number:"))
+print(x + y)
+
+# case 4
+x = int(input
+        ("enter first number:"))
+y = int(input
+        ("enter second number:"))   
+print(x + y)
+
+ 
