2e05b55d0a0897f002a8b4b9f2f244981facf078
salimatif <atifsalim992@gmail.com>
2025-11-18 18:32:35 +0530

    dataypr

--- atif.py
+++ atif.py
@@ -1,4 +1,4 @@
-# hay sir please dont delete this line
+# USE SEP, END and (\n)
 print("hello \nworld")
 print("hay i become a \"python devloper\" \nfirstly i will trying to consistantly learn python") 
 print("hello world",8083708992, 992, 992345, sep=" ", end="992\n")
@@ -7,3 +7,26 @@ print("the psyc atif")
 #the art of not overthinking
 print("shaurya kapoor the author of the art of not overthinking \nthe art of not overthinking is a great book")
 print("the art of not overthinking" , "is" ,70 , "pages longer book", 100,"fun and understanding book \nfor overthinker" )
+print("kingdom of the personally")
+
+# day 6
+# variables and detatype
+a = 8083708992
+print (a)
+name = ("atif salim")
+print(name)
+c = None 
+print ("the type of a is", type(a))
+print ("the type of name is", type(name))
+print ("the type of c is", type(c))
+
+#sequence deta type (LIST mutable) (TUPLE immutable)
+list = [8, 8.9, [334, 5673], ["atif", "salim",]]
+print (list)
+
+tuple = (8, 8.9, (334, 5673), ("atif", "salim",))
+print (tuple) 
+
+#mapped data type (DICTIONARY mutable)
+dict = {"name": "atif salim", "age": 20, "can_vote": True}
+print (dict)
