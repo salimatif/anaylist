3cf09f61b82f2dd1f83dba60a497cd84db0658a5
salimatif <atifsalim992@gmail.com>
2025-11-18 21:00:50 +0530

    typecasting

--- calculator.py
+++ calculator.py
@@ -8,3 +8,24 @@ print (20//4)
 print (7%3)
 print (2**4)
 print (15+4*2)
+
+# create calculator SOLUTION 
+a = 54
+b = 34 
+
+print("the valu of ", a, "+", 34, "is:", a+b)
+print("the valu of ", a, "-", 34, "is:", a-b)
+print("the valu of ", a, "*", 34, "is:", a*b)
+print("the valu of ", a, "/", 34, "is:", a/b) 
+ 
+ # typecasting in python
+a = 1
+b = 2
+print(str(a) +str(b))
+
+
+
+
+
+
+
