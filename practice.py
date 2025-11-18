4e83f0636393deeee5659def55bf8b8d5006368c
salimatif <atifsalim992@gmail.com>
2025-11-18 22:47:51 +0530

    practice

---
+++ practice.py
@@ -0,0 +1,27 @@
+# print("practice start for 10 days study ")
+
+print("first number", 
+ 5, 6, 7, 
+      sep="-", end="\n")
+print("second number",
+       8, 9, 10, sep="*", end="king ") 
+print("third number", 11, 12, 13, sep="&", end=
+      "jis college of engineering")
+
+# variables and data types practice
+a = 123
+b = "hello world"
+c = 45.67
+d = True
+e = None
+print("value of a:", a,
+       "type of a:", type(a))
+print("value of b:"
+      , b, "type of b:", type(b))
+print("value of c:", c, "type of c:", 
+      type(c))
+print("value of d:",
+       d, "type of d:", type(d))
+print("value of e:",
+      e, "type of e:", type(e)
+      )
